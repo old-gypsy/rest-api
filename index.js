b7079632d69d136875473e1af439be47fadcf143
Arafin Mridul <mridularafin@gmail.com>
2024-05-13 00:01:37 +0600

    routes to be added

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from "./routes/users.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use("/users", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+
+// * is wildcard character, runs for each type of HTTP requests
+app.all("*", (req, res) =>
+  res.send("You've tried reaching a route that doesn't exist.")
+);
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
